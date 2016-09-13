bdaf83595fa31ce17002c81ff92dbb5ff1f710f1
Mike Duffey <mpduffey@gmail.com>
2016-09-13 05:46:28 +0000

    "Fix

--- nav-menu.ts
+++ nav-menu.ts
@@ -8,8 +8,8 @@ import {ModalService}													from 'modules/modal/modal-service';
 		<div class="header" [ngClass]="{'header-fixed': pageYOffset>0, 'header-prepare': pageYOffset>100}">
 			<div class="header-inner">
 				<div class="logo"><a href="#"><img src="img/logo.png" /></a></div>
-				<div class="nav-menu-icon"><a><i class="fa fa-bars"></i></a></div>
-				<div class="nav-menu singlepage-nav">
+				<div class="nav-menu-icon" [ngClass]="{active: menuActive}"><a (click)="menuActive=!menuActive"><i class="fa fa-bars"></i></a></div>
+				<div class="nav-menu singlepage-nav" [ngClass]="{active: menuActive}">
 					<ul class="nav-menu-inner">
 						<li *ngFor="let item of menu"><a [href]="item.link" (click)="smoothScroll($event, item.external)" [attr.target]="item.external?'_blank':null">{{item.label}}</a></li>
 						<li *ngIf="showLogin"><a (click)="login($event)">Login</a></li>
@@ -24,7 +24,18 @@ import {ModalService}													from 'modules/modal/modal-service';
 		.header {position: absolute; text-align: center; top: 0px; z-index: 1001; color: #fff; width: 100%; height: 99px; transition: background-color 200ms ease-in-out 0s; -o-transition: background-color 200ms ease-in-out 0s; -moz-transition: background-color 200ms ease-in-out 0s; -webkit-transition: background-color 200ms ease-in-out 0s;}
 		.header.header-prepare {background-color: rgba(51, 51, 51, 0.90);}
 		.header.header-fixed {display: block; margin-top: 0 !important; position: fixed; height: 65px;}
+		.header-fixed .nav-menu ul.nav-menu-inner li a {padding: 26px 12px;}
+		.header-fixed .logo a {margin-top: 15px; margin-bottom: 15px; width: 160px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
 		.header-inner {padding-left: 30px; padding-right: 30px; position: relative;}
+		.nav-menu-icon {display: none; float: right;}
+		.nav-menu-icon a {color: #fff; display: block; font-size: 17px; padding: 19px 0;}
+		.nav-menu-icon.active a, .nav-menu-icon a.active, .nav-menu-icon:hover a, .nav-menu-icon a:hover, .nav-menu ul.nav-menu-inner li.active a, .nav-menu ul.nav-menu-inner li a.active, .nav-menu ul.nav-menu-inner li:hover a, .nav-menu ul.nav-menu-inner li a:hover, .header-fixed .nav-menu ul.nav-menu-inner li a.current, .header-fixed .nav-menu ul.nav-menu-inner li.current a {color: #c7a674;}
+		.header-fixed .nav-menu ul.nav-menu-inner li a {padding: 26px 12px;}
+		.nav-menu {position: static; float: right; display: block;}
+		.nav-menu.active {display: block;}
+		.nav-menu ul.nav-menu-inner {margin: 0; padding: 0; list-style: none;}
+		.nav-menu ul.nav-menu-inner li {display: inline-block; list-style: none; text-align: center;}
+		.nav-menu ul.nav-menu-inner li a {display: block; color: #fff; font-size: 13px; line-height: 1; padding: 43px 12px; font-weight: 700; text-transform: uppercase; letter-spacing: 1px; font-family: "Raleway",sans-serif;}
 		@media all and (max-width: 1024px) {
 			.header {height: 65px;}
 			.header .logo a {margin-top: 15px; margin-bottom: 15px; width: 160px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
@@ -36,15 +47,6 @@ import {ModalService}													from 'modules/modal/modal-service';
 		.logo {float: left; text-align: left; display: inline-block;}
 		.logo a {width: 180px; display: block; margin-top: 30px; margin-bottom: 30px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
 		.logo a img {width: 100%; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
-		.nav-menu-icon {display: none; float: right;}
-		.nav-menu-icon a {color: #fff; display: block; font-size: 17px; padding: 19px 0;}
-		.nav-menu-icon.active a, .nav-menu-icon a.active, .nav-menu-icon:hover a, .nav-menu-icon a:hover, .nav-menu ul.nav-menu-inner li.active a, .nav-menu ul.nav-menu-inner li a.active, .nav-menu ul.nav-menu-inner li:hover a, .nav-menu ul.nav-menu-inner li a:hover, .header-fixed .nav-menu ul.nav-menu-inner li a.current, .header-fixed .nav-menu ul.nav-menu-inner li.current a {color: #c7a674;}
-		.header-fixed .nav-menu ul.nav-menu-inner li a {padding: 26px 12px;}
-		.nav-menu {position: static; float: right; display: block;}
-		.nav-menu.active {display: block;}
-		.nav-menu ul.nav-menu-inner {margin: 0; padding: 0; list-style: none;}
-		.nav-menu ul.nav-menu-inner li {display: inline-block; list-style: none; text-align: center;}
-		.nav-menu ul.nav-menu-inner li a {display: block; color: #fff; font-size: 13px; line-height: 1; padding: 43px 12px; font-weight: 700; text-transform: uppercase; letter-spacing: 1px; font-family: "Raleway",sans-serif;}
 	`]
 })
 
@@ -67,6 +69,7 @@ export class NavMenu {
 	smoothScroll = (e, ext) => {
 		if(!ext) {
 			e.preventDefault();
+			this.menuActive = false;
 			$('html,body').scrollTo(e.target.hash, 1000); 
 		}
 	}
