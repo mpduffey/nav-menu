63506dfa5d2042d23ebc638b19dad2521326b48a
Mike Duffey <mpduffey@gmail.com>
2016-09-14 04:25:50 +0000

    NotSure

--- nav-menu.ts
+++ nav-menu.ts
@@ -7,7 +7,7 @@ import {ModalService}													from 'modules/modal/modal-service';
 	template:			`
 		<div class="header" [ngClass]="{'header-fixed': pageYOffset>0, 'header-prepare': pageYOffset>100}">
 			<div class="header-inner">
-				<div class="logo"><a href="#"><img src="img/logo.png" /></a></div>
+				<div class="logo"><a href="#"><img src="img/logo-w.png" /></a></div>
 				<div class="nav-menu-icon" [ngClass]="{active: menuActive}"><a (click)="menuActive=!menuActive"><i class="fa fa-bars"></i></a></div>
 				<div class="nav-menu singlepage-nav" [ngClass]="{active: menuActive}">
 					<ul class="nav-menu-inner">
@@ -25,7 +25,7 @@ import {ModalService}													from 'modules/modal/modal-service';
 		.header.header-prepare {background-color: rgba(51, 51, 51, 0.90);}
 		.header.header-fixed {display: block; margin-top: 0 !important; position: fixed; height: 65px;}
 		.header-fixed .nav-menu ul.nav-menu-inner li a {padding: 26px 12px;}
-		.header-fixed .logo a {margin-top: 15px; margin-bottom: 15px; width: 160px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
+		.header-fixed .logo a {margin-top: 15px; margin-bottom: 15px; width: 260px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
 		.header-inner {padding-left: 30px; padding-right: 30px; position: relative;}
 		.nav-menu-icon {display: none; float: right;}
 		.nav-menu-icon a {color: #fff; display: block; font-size: 17px; padding: 19px 0;}
@@ -38,14 +38,14 @@ import {ModalService}													from 'modules/modal/modal-service';
 		.nav-menu ul.nav-menu-inner li a {display: block; color: #fff; font-size: 13px; line-height: 1; padding: 43px 12px; font-weight: 700; text-transform: uppercase; letter-spacing: 1px; font-family: "Raleway",sans-serif;}
 		@media all and (max-width: 1024px) {
 			.header {height: 65px;}
-			.header .logo a {margin-top: 15px; margin-bottom: 15px; width: 160px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
+			.header .logo a {margin-top: 15px; margin-bottom: 15px; width: 260px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
 			.nav-menu ul.nav-menu-inner li {display: block; width: 100%;}
 			.nav-menu {position: fixed; top: 65px; left: 0; float: none; display: block; width: 100%; background: #333; overflow-y: auto; display: none; z-index: 999;}
 			.nav-menu-icon, .header-fixed .nav-menu-icon {display: inline-block; float: right;}
 			.nav-menu ul.nav-menu-inner li a, .header-fixed .nav-menu ul.nav-menu-inner li a {padding: 20px 12px; border-top: 1px solid #444;}
 		}
 		.logo {float: left; text-align: left; display: inline-block;}
-		.logo a {width: 180px; display: block; margin-top: 30px; margin-bottom: 30px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
+		.logo a {width: 280px; display: block; margin-top: 30px; margin-bottom: 30px; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
 		.logo a img {width: 100%; transition: all 0.3s ease 0s; -o-transition: all 0.3s ease 0s; -moz-transition: all 0.3s ease 0s; -webkit-transition: all 0.3s ease 0s;}
 	`]
 })
